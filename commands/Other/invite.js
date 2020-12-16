7af3a80c90f63eb4daaa0a7225b5c4ef364c0c4a
Glitch (clouds-music) <none>
2020-12-16 21:36:51 +0000

    updated

---
+++ commands/Other/invite.js
@@ -0,0 +1,26 @@
+const { Default_Prefix, Color, Owner, Support, Donate } = require("../../config.js");
+const Discord = require("discord.js");
+const db = require("wio.db");
+
+module.exports = {
+  name: "invite",
+  aliases: ["invitelink"],
+  category: "Other",
+  description: "Give You My Invite Link, Etc!",
+  usage: "Invite",
+  run: async (client, message, args) => {
+    
+    const Invite = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`, Owne = `<@${Owner}>`, Dev = `Trent#0013`;
+    
+    const Embed = new Discord.MessageEmbed()
+    .setColor(Color)
+    .setTitle("Thanks")
+    .addField("Invite Me", `[Click Me](${Invite})`, true)
+    .addField("Support Server", `[Click Me](${Support})`, true)
+    .addField("Owner", Owne, true)
+    .addField("Developer", Dev)
+    .setTimestamp();
+    
+    return message.channel.send(Embed).catch(() => message.channel.send("Invite Link - " + Invite));
+  }
+};
---
+++ config.js
@@ -0,0 +1,8 @@
+exports.Token = "Nzg2ODYxMjE0ODYwNDQzNjQ4.X9MjnA.35pPQNc6DFT0Cc76FGIUQlAELpw"; //Bot Token - Important
+exports.Owner = "754385212590522448"; //Bot Owner ID - Important
+exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "BLACK"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/VZ2GjfrVwp`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Donate = ``; //Please Don't Change It, If You Want To Support Me <3
+
+
